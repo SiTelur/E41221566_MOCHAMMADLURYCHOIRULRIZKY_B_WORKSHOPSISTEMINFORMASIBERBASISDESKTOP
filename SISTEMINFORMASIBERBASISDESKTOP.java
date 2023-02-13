1b9ed356fc308a22350fa23e98573e0643c48e82
SiTelur <gantengrizky440@gmail.com>
2023-02-13 10:33:18 +0700

    Create SISTEMINFORMASIBERBASISDESKTOP.java

---
+++ SISTEMINFORMASIBERBASISDESKTOP.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sisteminformasiberbasisdesktop;
+
+/**
+ *
+ * @author User
+ */
+public class SISTEMINFORMASIBERBASISDESKTOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("E41221566");
+        System.out.println("MOCHAMMAD LURY CHOIRUL RIZKY");
+        System.out.println("GOLONGAN A");
+    }
+    
+}
